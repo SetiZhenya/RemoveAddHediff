49308f82810fa3ccd1b25f2729ec6a8f37d1e4c0
SetiZhenya <77733838+SetiZhenya@users.noreply.github.com>
2024-12-30 20:53:31 +0200

    Add files via upload

---
+++ ExampleDllMod-master/RimWorld_ExampleProjectDLL/Recipe_RemoveAddHediff.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Verse;
+
+namespace RimWorldSurgeryAddRemove;
+
+public class Recipe_RemoveAddHediff : Recipe_Surgery
+{
+    public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
+    {
+        return IsValidNow(thing, part);
+    }
+
+    public bool IsValidNow(Thing thing, BodyPartRecord part = null, bool ignoreBills = false)
+    {
+        if (!base.AvailableOnNow(thing, part))
+        {
+            return false;
+        }
+
+        if (!(thing is Pawn pawn))
+        {
+            return false;
+        }
+
+        if (part != null && (pawn.health.WouldDieAfterAddingHediff(recipe.addsHediff, part, 1f) || pawn.health.WouldLosePartAfterAddingHediff(recipe.addsHediff, part, 1f)))
+        {
+            return false;
+        }
+
+        if (pawn.health.hediffSet.HasHediff(recipe.addsHediff))
+        {
+            return false;
+        }
+
+        if (!ignoreBills && pawn.BillStack.Bills.Any((Bill b) => b.recipe == recipe))
+        {
+            return false;
+        }
+
+        if (pawn.health.hediffSet.hediffs.Any((Hediff x) => !recipe.CompatibleWithHediff(x.def)))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+    {
+        if (billDoer != null)
+        {
+            if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
+            {
+                return;
+            }
+
+            TaleRecorder.RecordTale(TaleDefOf.DidSurgery, billDoer, pawn);
+        }
+
+        pawn.health.AddHediff(recipe.addsHediff, part);
+        OnSurgerySuccess(pawn, part, billDoer, ingredients, bill);
+        if (IsViolationOnPawn(pawn, part, Faction.OfPlayerSilentFail))
+        {
+            ReportViolation(pawn, billDoer, pawn.HomeFaction, -70);
+        }
+
+        Hediff hediff = pawn.health.hediffSet.hediffs.Find((Hediff x) => x.def == recipe.removesHediff && x.Part == part && x.Visible);
+        if (hediff != null)
+        {
+            pawn.health.RemoveHediff(hediff);
+        }
+    }
+}
